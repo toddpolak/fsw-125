3b78db483b3d5fbad9a42482e3c0a887c988e320
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-12-19 21:51:38 -0800

    Math

--- week1/math/app.js
+++ week1/math/app.js
@@ -1,10 +1,9 @@
 const math = require('./math')
 const num1 = 144
 const num2 = 17
-const num3 = 4
-const num4 = 5
-const num5 = 6
-const num6 = 7
+const num3 = 5
+const num4 = 6
+const num5 = 7
 
 console.log(`The sum of ${num1} and ${num2} is ${math.add(num1, num2)}`)
 
@@ -12,8 +11,8 @@ console.log(`${num1} minus ${num2} is ${math.subtract(num1, num2)}`)
 
 console.log(`${num1} multiplied by ${num2} is ${math.multiply(num1, num2)}`)
 
-console.log(`${num1} divided by ${num2} is ${math.divide(num1, num2)}`)
+console.log(`${num2} divided by ${num3} is ${math.divide(num2, num3)} or ${math.divide(num2, num3, false)}`)
 
 console.log(`The square root of ${num1} is ${math.squareRoot(num1)}`)
 
-console.log(`The area of a triangle with sides ${num4}, ${num5} and ${num6} is ${math.areaOfTriangle(num4, num5, num6)}`)
+console.log(`The area of a triangle with sides ${num3}, ${num4} and ${num5} is ${math.areaOfTriangle(num3, num4, num5)}`)
