35a87296885b593b30f44d41d8fd3b257b88a2d8
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-12-19 21:45:19 -0800

    Math

--- week1/math/app.js
+++ week1/math/app.js
@@ -1,6 +1,10 @@
 const math = require('./math')
-const num1 = 188
+const num1 = 144
 const num2 = 17
+const num3 = 4
+const num4 = 5
+const num5 = 6
+const num6 = 7
 
 console.log(`The sum of ${num1} and ${num2} is ${math.add(num1, num2)}`)
 
@@ -9,3 +13,7 @@ console.log(`${num1} minus ${num2} is ${math.subtract(num1, num2)}`)
 console.log(`${num1} multiplied by ${num2} is ${math.multiply(num1, num2)}`)
 
 console.log(`${num1} divided by ${num2} is ${math.divide(num1, num2)}`)
+
+console.log(`The square root of ${num1} is ${math.squareRoot(num1)}`)
+
+console.log(`The area of a triangle with sides ${num4}, ${num5} and ${num6} is ${math.areaOfTriangle(num4, num5, num6)}`)
--- week1/math/math.js
+++ week1/math/math.js
@@ -1,17 +1,54 @@
-module.exports.add = (a, b) => {
+add = (a, b) => {
     return a + b
 }
 
-module.exports.subtract = (a, b) => {
+subtract = (a, b) => {
     return a - b
 }
 
-module.exports.multiply = (a, b) => {
+multiply = (a, b) => {
     return a * b
 }
 
-module.exports.divide = (a, b) => {
-    return (a % b) === 0 
+divide = (a, b, remainder = true) => {
+
+    if (remainder) {
+        return (a % b) === 0 
         ? a / b
         : `${Math.floor(a / b)} with a remainder of ${a % b}`
+    } else {
+        return a / b
+    }
+}
+
+squareRoot = (a) => {
+    return Math.sqrt(a)
+}
+
+areaOfTriangle = (a, b, c) => {
+    let sides, area, perimeter, x, y, z
+
+    sides = add(a, b)
+    sides = add(sides, c)
+
+    perimeter = divide(sides, 2, false)
+
+    x = subtract(perimeter, a)
+    y = subtract(perimeter, b)
+    z = subtract(perimeter, c)
+
+    area = multiply(x, y)
+    area = multiply(area, z)
+    area = multiply(perimeter, area)
+
+    return squareRoot(area)
+}
+
+module.exports = { 
+    add,
+    subtract,
+    multiply,
+    divide,
+    squareRoot,
+    areaOfTriangle
 }
