209a048b6d9eb625ae314b83bffaae1a9bab274e
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-01-02 21:39:32 -0800

    Express Intro

--- week2/express-intro/project/app.js
+++ week2/express-intro/project/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+const users = [
+    { name: 'Todd', age: 48},
+    { name: 'Joe', age: 26},
+    { name: 'Jane', age: 30}
+]
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
 app.listen(3000, () => {
     console.log("App is listening on port 3000!");
 });
