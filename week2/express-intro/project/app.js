3f367f9d899babbbca63ddd859479d11cac76f29
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-01-10 00:56:54 -0800

    Express Intro

--- week2/express-intro/project/app.js
+++ week2/express-intro/project/app.js
@@ -1,5 +1,8 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
+
+//Middleware (for every request) //
+app.use('/', express.json()) // Looks for a request body, and turns it into 'req.body'
 
 const users = [
     { name: 'Todd', age: 48},
@@ -7,9 +10,16 @@ const users = [
     { name: 'Jane', age: 30}
 ]
 
-app.get('/users', (req, res) => {
-    res.send(users);
+app.get("/users", (req, res) => {
+    res.send(users)
 });
+
+app.post("/users", (req, res) => {
+    //console.log(req)
+    const newUser = req.body
+    users.push(newUser)
+    res.send(`Successfully added ${newUser.name} to database!`)
+})
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000!");
