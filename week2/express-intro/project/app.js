afbec1260d251efd57a512b435e34716881bef53
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-01-10 18:45:42 -0800

    Express Intro

--- week2/express-intro/project/app.js
+++ week2/express-intro/project/app.js
@@ -1,22 +1,24 @@
-const express = require("express")
+const express = require('express')
 const app = express()
+const { v4: uuidv4 } = require('uuid')
 
 //Middleware (for every request) //
 app.use('/', express.json()) // Looks for a request body, and turns it into 'req.body'
 
 const users = [
-    { name: 'Todd', age: 48},
-    { name: 'Joe', age: 26},
-    { name: 'Jane', age: 30}
+    { name: 'Todd', age: 48, _id: uuidv4() },
+    { name: 'Joe', age: 26, _id: uuidv4() },
+    { name: 'Jane', age: 30, _id: uuidv4() }
 ]
 
 app.get("/users", (req, res) => {
     res.send(users)
 });
 
-app.post("/users", (req, res) => {
-    //console.log(req)
+app.post('/users', (req, res) => {
+    console.log(req)
     const newUser = req.body
+    newUser._id = uuidv4()
     users.push(newUser)
     res.send(`Successfully added ${newUser.name} to database!`)
 })
