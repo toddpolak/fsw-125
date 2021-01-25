172e752f2d5e86ec206ca176a8deb05c88f3f85b
toddpolak <todd.polak@bryanuniversity.edu>
2021-01-25 08:55:12 -0800

    Capstone Project

---
+++ week7/capstone-project/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+// Middleware (for every request)
+app.use('/', express.json())
+app.use(morgan('dev'))
+
+// Routes
+app.use('/data', require('./routes/router'))
+
+// Error handler
+app.use((err, req, res, next) => {
+
+})
+
+// Server Listen
+app.listen(9000, () => {
+    console.log('The server is listening on port 9000!');
+});
